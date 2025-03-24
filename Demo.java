a8dd7d370be29b47373a482d525911e4d66ab058
Teja <tejamosuru.w019@gmailcom>
2025-03-24 22:25:49 +0530

    this is my first commit

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Demo{
     public static void main(String[] args){
         System.out.println("Hello Teja");
-        System.out.println("I am typing...");
+        System.out.println("I am typing.......");
     }
 }
